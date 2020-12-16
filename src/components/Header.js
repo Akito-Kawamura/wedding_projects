4072b1e8bb136773a5202d020dbc72c771607b9f
Akito-Kawamura <45854488+Akito-Kawamura@users.noreply.github.com>
2020-12-17 03:33:24 +0900

    add headerName

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,47 @@
 import React from "react";
-import { AppBar, IconButton } from "material-ui";
-import FaceIcon from "material-ui/svg-icons/action/face";
-const Header = () => {
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+// import HeaderMenu from "./molecules/Header/HeaderMenu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
   return (
-    <header style={styles.header}>
-      <AppBar
-        style={styles.header_bar}
-        title="Taro & Hanako"
-        iconElementRight={
-          <IconButton>
-            <FaceIcon />
+    <header className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
           </IconButton>
-        }
-      />
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </header>
   );
-};
-export default Header;
-const styles = {
-  header: {
-    textAlign: "center",
-    color: "#FFFFFF",
-    fontSize: "5vh",
-    // background: "black",
-    height: "10vh"
-  },
-  header_bar: {
-    height: "100%",
-    background: "black"
-  }
-};
+}
